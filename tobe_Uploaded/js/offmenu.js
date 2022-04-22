658bdb4b1acbeb570c0c4226c6dd6298dfcadc16
Ronald Dale Fuentebella <72976699+ronaldxdale09@users.noreply.github.com>
2022-04-22 16:26:59 +0800

    initial commit

--- tobe_Uploaded/js/offmenu.js
+++
@@ -1,8 +0,0 @@
-(function () {
-    'use strict'
-  
-    document.querySelector('[data-bs-toggle="offcanvas"]').addEventListener('click', function () {
-      document.querySelector('.dropdown-menu').classList.toggle('open')
-    })
-  })()
-  

9f120f9846518560f87da76560b03be241392702
Ronald Dale Fuentebella <72976699+ronaldxdale09@users.noreply.github.com>
2022-04-26 22:55:43 -0700

    updated

--- admission-officer/modal/modal.php
+++ admission-officer/modal/modal.php
@@ -1,28 +1,44 @@
 <!-- SCORE ACTION -->
+<<<<<<< Updated upstream
 <div class="modal fade" id="newInterviewer" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+=======
+<div class="modal fade" id="settings" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+>>>>>>> Stashed changes
     aria-hidden="true">
     <div class="modal-dialog" role="document">
         <div class="modal-content">
             <div class="modal-header">
+<<<<<<< Updated upstream
                 <h5 class="modal-title" id="exampleModalLabel">INTERVIEW SCORE</h5>
+=======
+                <h5 class="modal-title" id="exampleModalLabel"> SETTINGS</h5>
+>>>>>>> Stashed changes
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                 </button>
             </div>
             <form action="function/update_settings.php" method="POST">
+<<<<<<< Updated upstream
 
                 <div class="modal-body">
 
 
                 <input type="name" name="id" id="course_id" hidden>
 
+=======
+                <div class="modal-body">
+                <input type="name" name="id" id="course_id" hidden>
+>>>>>>> Stashed changes
                     <div class="form-group">
                         <label>Course :</label>
                         <input type="name" name="course" id="course_name" class="form-control" readonly>
                     </div>
+<<<<<<< Updated upstream
 
 
 
+=======
+>>>>>>> Stashed changes
                     <div class="row">
                         <div class="col-md-6">
                             <label for="IDofInput">CET REQUIREMENTS</label>
@@ -33,7 +49,10 @@
                             <input type="number" name="gpa" id="gpa" class="form-control" >
                         </div>
                     </div>
+<<<<<<< Updated upstream
 
+=======
+>>>>>>> Stashed changes
                     <div class="row">
                         <div class="col-md-6">
                             <label for="IDofInput">SLOTS</label>
@@ -44,6 +63,7 @@
                             <input type="name" name="fname" id="fname1" class="form-control" > -->
                         </div>
                     </div>
+<<<<<<< Updated upstream
 
 
 
@@ -68,4 +88,17 @@
 </div>
 
 
+=======
+                    <hr> <br>
+                    <!-- attached -->
+                </div>
+                <div class="modal-footer">
+                    <button type="accept" name="update" class="btn btn-success">UPDATE</button>
+                    <button type="button" class="btn btn-danger" data-dismiss="modal">CLOSE</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
+>>>>>>> Stashed changes
 <!-- ASSIGN SCHEDULE AND INSTRUCTOR -->
